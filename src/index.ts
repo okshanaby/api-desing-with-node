e9ee2b05439499d98faf75c341c8b69be06bdf18
okshanaby <steamnature006@gmail.com>
2025-06-24 23:25:35 +0300

    3. Prisma Overview

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@
 //   console.log("Server is running on http://localhost:3001");
 // });
 
-const app = require("./server");
+import app from './server'
 
 app.listen(3001, () => {
   console.log("Server is running on http://localhost:3001");
