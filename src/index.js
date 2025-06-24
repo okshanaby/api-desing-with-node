9144a944963867aa702705f346ed1fe189f36168
okshanaby <steamnature006@gmail.com>
2025-06-24 22:55:03 +0300

    4. Creating a Server with Express

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-const http = require("http");
+// const http = require("http");
 
-const server = http.createServer((req, res) => {
-  if (req.method === "GET" && req.url === "/") {
-    console.log("Hello from the server");
-    res.end();
-  } else {
-    console.log("URL: " + req.url);
-    res.end();
-  }
-});
+// const server = http.createServer((req, res) => {
+//   if (req.method === "GET" && req.url === "/") {
+//     console.log("Hello from the server");
+//     res.end();
+//   } else {
+//     console.log("URL: " + req.url);
+//     res.end();
+//   }
+// });
 
-server.listen(3001, () => {
+// server.listen(3001, () => {
+//   console.log("Server is running on http://localhost:3001");
+// });
+
+const app = require("./server");
+
+app.listen(3001, () => {
   console.log("Server is running on http://localhost:3001");
 });
---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("Express API");
+
+  res.status(200);
+  res.json({ message: "Hello from the server" });
+});
+
+module.exports = app;
