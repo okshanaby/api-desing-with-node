d9bb22425ccbb9d05dd67c3b86508c33b964ed7f
okshanaby <steamnature006@gmail.com>
2025-06-25 00:53:20 +0300

    4. Middleware

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import router from "./routes";
+import morgan from 'morgan'
 
 const app = express();
+
+app.use(morgan('dev'))
 
 app.get("/", (req, res) => {
   console.log("Express API");
@@ -10,6 +13,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello from the server" });
 });
 
+
 app.use('/api', router)
+
 
 export default app;
