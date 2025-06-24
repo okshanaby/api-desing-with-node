38da169d24dc614c9b240cc61e10d3bf5648d739
okshanaby <steamnature006@gmail.com>
2025-06-25 01:10:44 +0300

    5. Creating a Custom Middleware

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,36 @@
 import express from "express";
+import morgan from "morgan";
 import router from "./routes";
-import morgan from 'morgan'
 
 const app = express();
 
-app.use(morgan('dev'))
+// middleware for logging
+app.use(morgan("dev"));
+
+app.use(express.json()); // middleware to let the client send a json
+app.use(express.urlencoded({ extended: true })); // middleware for url params/queries formatting
+
+// app.use((req, res, next) => {
+//   req.body = "Okshan";
+//   next();
+// });
+
+const customLogger = message => (req, res, next) => {
+  console.log(`Hello from my loger ${message}`)
+
+  next();
+};
+
+app.use(customLogger('Okshan'))
 
 app.get("/", (req, res) => {
   console.log("Express API");
 
   res.status(200);
-  res.json({ message: "Hello from the server" });
+  // res.json({ message: "Hello from the server " + req.body });
+  res.json({ message: "Hello from the server " });
 });
 
-
-app.use('/api', router)
-
+app.use("/api", router);
 
 export default app;
