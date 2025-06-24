288cfe844f82c5d3f66808c6ee631be77d13e3e7
okshanaby <steamnature006@gmail.com>
2025-06-25 00:35:11 +0300

    1. Defining Routes

---
+++ src/routes.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+
+const router = Router();
+
+// PRODUCT ROUTES
+router.get("/product", (req, res) => {});
+router.post("/product", (req, res) => {});
+router.get("/product/:id", (req, res) => {});
+router.put("/product/:id", (req, res) => {});
+router.delete("/product/:id", (req, res) => {});
+
+// UPDATE ROUTES
+router.get("/update", (req, res) => {});
+router.post("/update", (req, res) => {});
+router.get("/update/:id", (req, res) => {});
+router.put("/update/:id", (req, res) => {});
+router.delete("/update/:id", (req, res) => {});
+
+// UPDATEPOINT ROUTES
+router.get("/updatepoint", (req, res) => {});
+router.post("/updatepoint", (req, res) => {});
+router.get("/updatepoint/:id", (req, res) => {});
+router.put("/updatepoint/:id", (req, res) => {});
+router.delete("/updatepoint/:id", (req, res) => {});
